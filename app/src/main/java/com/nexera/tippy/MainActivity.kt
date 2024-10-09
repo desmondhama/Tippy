0aa7b4d20163921c3948aef750cf684f32402996
desmondhama <desmondhama@gmail.com>
2024-10-09 03:39:36 +0200

    TEST commit massage

---
+++ app/src/main/java/com/nexera/tippy/MainActivity.kt
@@ -0,0 +1,29 @@
+
+package com.nexera.tippy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nexera.tippy.databinding.ActivityMainBinding
+
+public class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    
+    private val binding: ActivityMainBinding
+      get() = checkNotNull(_binding) { "Activity has been destroyed" }
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Inflate and get instance of binding
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+
+        // set content view to binding's root
+        setContentView(binding.root)
+    }
+    
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
